a1b8cd58e1190b2af211bf4450b5e9f175093b7c
Otavio Cunha <otaviocunhaps1@gmail.com>
2024-02-21 12:42:44 -0300

    pagina de twitter adicionada

---
+++ src/projectsPages/Twitter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./cssPgs/projects.css";
+
+
+const Twitter = () => {
+  return (
+    <body>
+      <div className="content">
+        <div className="content-projects">
+          <section className="title">
+            <h1>Twitter</h1>
+          </section>
+
+          </div>
+      </div>
+    </body>
+  );
+};
+
+export default Twitter;
